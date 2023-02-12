2beb0de39d140db44e7a6d0f2e5678b4ba0b7c46
Edyane <edyanealves@gmail.com>
2023-02-12 20:51:51 -0300

    Inserindo listener de interações com o BOT

--- index.js
+++ index.js
@@ -3,12 +3,37 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 //dotenv
 const dotenv = require('dotenv');
 dotenv.config();
-const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+const { TOKEN } = process.env;
+
+//importação dos comandos
+const fs = require("node:fs");
+const path = require("node:path");
+
+const commandsPath = path.join(__dirname, "commands");
+const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
 
+for (const file of commandsFiles) {
+	const filePath = path.join(commandsPath, file)
+	const command = require(filePath)
+	if ("data" in command && "execute" in command) {
+		client.commands.set(command.data.name, command)
+	} else {
+		console.log(`Esse comando em ${file.path} está com "data" ou "execute" ausentes!`)
+	}
+}
+
+//login do Bot
 client.once(Events.ClientReady, c => {
 	console.log(`Login realizado como ${c.user.tag}`);
 });
 
 client.login(TOKEN);
+
+//listener de interações com o bot
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand ()) return
+	console.log(interaction) 
+})
