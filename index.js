05e38cc522e4bba97c3c474adbd50946e4e17f07
Edyane <edyanealves@gmail.com>
2023-02-12 20:12:13 -0300

    Adicionando dotenv ao projeto

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-// Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 
-// Create a new client instance
+//dotenv
+const dotenv = require('dotenv');
+dotenv.config();
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+	console.log(`Login realizado como ${c.user.tag}`);
 });
 
-// Log in to Discord with your client's token
-client.login(token);
+client.login(TOKEN);
