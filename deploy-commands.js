426aa401b4fc6b4066768548fb4a705e9b13a56d
Edyane <edyanealves@gmail.com>
2023-02-13 20:19:25 -0300

    Construindo arquivo deploy-command.js

---
+++ deploy-commands.js
@@ -0,0 +1,22 @@
+const { REST, Routes } = require ("discord.js")
+
+//dotenv
+const dotenv = require('dotenv');
+dotenv.config();
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+
+//importação dos comandos
+const fs = require("node:fs");
+const path = require("node:path");
+const commandsPath = path.join(__dirname, "commands");
+const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+const commands = []
+
+for (const file in commandsFiles) {
+    const command = require(`./commands/${file}`)
+    commands.push(command.data.toJSON())
+}
+
+//instância REST
+const rest = new REST ({version: "10"}).setToken(TOKEN)
